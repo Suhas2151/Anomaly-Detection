0700daf10a02eb3788fd8eb2b7c07af8fcbc3422
Suhas2151 <136999431+Suhas2151@users.noreply.github.com>
2023-08-29 19:10:38 +0530

    Update Anomaly_main.py

--- Anomaly_main.py
+++ Anomaly_main.py
@@ -3,43 +3,61 @@ import pandas as pd
 import re
 import  pickle
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+import re
 import ipaddress
+from datetime import datetime
 # Load the trained Random Forest model
 loaded_random_forest_model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
 # Function to preprocess the log data
-def preprocess_log(log_text,protocol, user_agent):
+def preprocess_log(log_text):
     ip_pattern = r'(^\S+\.[\S+\.]+\S+)\s'
     timestamp_pattern = r'\[(\d{2}/\w+/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\]'
     request_pattern = r'\"(\S+)\s(\S+)\s*(\S*)\"'
     status_pattern = r'(\d{3})'
     size_pattern = r'\s(\d+) "'
-    user_agent_pattern = r'"([^"]+)"'
+    # user_agent_pattern = r'"([^"]+)"'
     protocol_pattern = r'HTTP/([\d.]+)'
-    browser_pattern = r'"([^"]+)"$'
-    
-    ip_str = re.search(ip_pattern, log_text).group(1)
+    # browser_pattern = r'"([^"]+)"$'
+    ip = re.search(ip_pattern, log_text).group(1)
     timestamp = re.search(timestamp_pattern, log_text).group(1)
     request_line = re.search(request_pattern, log_text).group(1)
-    status_code = re.search(status_pattern, log_text).group(1)
-    size = re.search(size_pattern, log_text).group(1)
-    # ... extract other features ...
+    protocol = re.search(protocol_pattern, log_text).group(1)
+    size=re.search(size_pattern, log_text).group(1)
+    status=re.search(status_pattern, log_text).group(1)
+    if protocol=='1.1':
+        protocol=6
     
-    # Apply label encoding to categorical features
-    protocol_encoder = LabelEncoder()
-    protocol_encoded = protocol_encoder.transform([protocol])[0]
-    user_agent_encoder = LabelEncoder()
-    user_agent_encoded = user_agent_encoder.transform([user_agent])[0]
-    # ... apply encoding for other features ...
+    if request_line=='GET':
+        request_line=4
+    elif request_line=='POST':
+        request_line=7
+    else:
+        request_line=1
+    def convert_ip_to_numeric(ip):
+        octets = ip.split('.')
+        numeric_value = 0
+        for i, octet in enumerate(octets):
+            numeric_value += int(octet) * (256 ** (3 - i))
+        return numeric_value
+
+
+    def extract_date_time_components(timestamp):
+        format_string = "%d/%b/%Y:%H:%M:%S %z"
+        parsed_date = datetime.strptime(timestamp, format_string)
+        
+        day=parsed_date.day,
+        hour=parsed_date.hour,
+        minute=parsed_date.minute,
+        second=parsed_date.second
     
-    # Convert IP address to numeric value
-   
-    def convert_ip_to_numeric(ip_str):
-        ip = ipaddress.ip_address(ip_str)
-    return int(ip)
+        
+        return day,hour,minute,second
     
-    # Return the processed features as a list
-    processed_features = [ip_numeric, timestamp, request_line, protocol_encoded, user_agent_encoded, ...]
+    ip_numeric=convert_ip_to_numeric(ip)
+    date, hour, minute, second=extract_date_time_components(timestamp)
+    request=1
+    processed_features = [ip_numeric,date,hour,minute,second,request_line,protocol,size,status,request]
     return processed_features
 
 # Streamlit UI
@@ -60,21 +78,26 @@ elif selected_tab == "Anomaly Detection":
     
     # User input for log text
     user_input = st.text_area("Enter log text:", "")
-    
     if st.button("Detect Anomaly"):
         if user_input:
             # Preprocess the user input
             processed_features = preprocess_log(user_input)
-            
-            # Apply scaling if needed
-            scaler = StandardScaler()
-            processed_features_scaled = scaler.transform([processed_features])
-            
-            # Make prediction using the loaded model
-            predicted_label = loaded_random_forest_model.predict(processed_features_scaled)[0]
-            
-            if predicted_label == 0:
-                st.write("Prediction: Normal")
+        input_list = processed_features
+
+   # Initialize an empty list to store all values
+        all_values_list = []
+
+        # Iterate through the elements and append them to the new list
+        for item in input_list:
+            if isinstance(item, tuple):
+                all_values_list.append(item[0])  # Append the value inside the tuple
             else:
-                st.write("Prediction: Anomaly")
+                all_values_list.append(item)  # Append the item as is
+
+        predicted_label = loaded_random_forest_model.predict([all_values_list])[0]
+                
+        if predicted_label == 0:
+            st.write("Prediction: Normal")
+        else:
+            st.write("Prediction: Anomaly")
 
