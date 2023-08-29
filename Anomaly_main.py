504a9701558f47c3308a0ef94dff185ea8724495
Suhas2151 <136999431+Suhas2151@users.noreply.github.com>
2023-08-29 14:21:50 +0530

    Update Anomaly_main.py

--- Anomaly_main.py
+++ Anomaly_main.py
@@ -1,14 +1,15 @@
 import streamlit as st
-import pandas as pd
 import re
-import  pickle
+import pickle
+import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 # Load the trained Random Forest model
 loaded_random_forest_model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
 # Function to preprocess the log data
-def preprocess_log(log_text,protocol, user_agent):
+def preprocess_log(log_text):
+    # Define regular expressions to extract relevant information
     ip_pattern = r'(^\S+\.[\S+\.]+\S+)\s'
     timestamp_pattern = r'\[(\d{2}/\w+/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\]'
     request_pattern = r'\"(\S+)\s(\S+)\s*(\S*)\"'
@@ -16,25 +17,30 @@ def preprocess_log(log_text,protocol, user_agent):
     size_pattern = r'\s(\d+) "'
     user_agent_pattern = r'"([^"]+)"'
     protocol_pattern = r'HTTP/([\d.]+)'
-    browser_pattern = r'"([^"]+)"$'
     
+    # Extract relevant features using regular expressions
     ip = re.search(ip_pattern, log_text).group(1)
-    timestamp = re.search(timestamp_pattern, log_text).group(1)
     request_line = re.search(request_pattern, log_text).group(1)
+    protocol = re.search(protocol_pattern, log_text).group(1)
+    status_code = re.search(status_pattern, log_text).group(1)
+    size = re.search(size_pattern, log_text).group(1)
     # ... extract other features ...
-    
-    # Apply label encoding to categorical features
-    protocol_encoder = LabelEncoder()
-    protocol_encoded = protocol_encoder.transform([protocol])[0]
-    user_agent_encoder = LabelEncoder()
-    user_agent_encoded = user_agent_encoder.transform([user_agent])[0]
-    # ... apply encoding for other features ...
     
     # Convert IP address to numeric value
     ip_numeric = convert_ip_to_numeric(ip)
     
-    # Return the processed features as a list
-    processed_features = [ip_numeric, timestamp, request_line, protocol_encoded, user_agent_encoded, ...]
+    # Extract date, hour, minute, seconds from the timestamp
+    # ... perform necessary extraction ...
+    
+    # Return the processed features as a dictionary
+    processed_features = {
+        'Ip_address': ip_numeric,
+        'HTTP request line': request_line,
+        'protocol': protocol,
+        'HTTP status code': status_code,
+        'Size of the response in bytes': size,
+        # ... add other features ...
+    }
     return processed_features
 
 # Streamlit UI
@@ -61,9 +67,12 @@ elif selected_tab == "Anomaly Detection":
             # Preprocess the user input
             processed_features = preprocess_log(user_input)
             
+            # Convert the processed features into a DataFrame
+            features_df = pd.DataFrame([processed_features])
+            
             # Apply scaling if needed
             scaler = StandardScaler()
-            processed_features_scaled = scaler.transform([processed_features])
+            processed_features_scaled = scaler.transform(features_df)
             
             # Make prediction using the loaded model
             predicted_label = loaded_random_forest_model.predict(processed_features_scaled)[0]
@@ -72,3 +81,4 @@ elif selected_tab == "Anomaly Detection":
                 st.write("Prediction: Normal")
             else:
                 st.write("Prediction: Anomaly")
+
